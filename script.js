b0cd6d5b3af6833e8572e04565f699fed9523926
meenuadhu <meenuadhu@gmail.com>
2021-04-20 12:25:18 +0530

    Frist Commit : version1

---
+++ script.js
@@ -0,0 +1,17 @@
+
+// // PHONE NUMBER
+
+
+// function phonenumber(inputtxt)
+// {
+//   var phoneno = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
+//   if(inputtxt.value.match(phoneno))
+//      {
+// 	   return true;      
+// 	 }
+//    else
+//      {
+// 	   alert("Not a valid Phone Number");
+// 	   return false;
+//      }
+// }
